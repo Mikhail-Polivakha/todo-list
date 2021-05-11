b5a305897670bc03f0c8c6540437614cf2701850
mikhail.polivakha <mikhail.polivakha@rt.ru>
2021-05-11 22:19:12 +0300

    append mongo dependency, also create unsuccessful servicelog persistence class using mongotemplate

---
+++ src/main/java/com/example/todolistapp/domain/fields/UnsuccessfulServiceLogField.java
@@ -0,0 +1,11 @@
+package com.example.todolistapp.domain.fields;
+
+public class UnsuccessfulServiceLogField {
+    public static final String COLLECTION_NAME = "unsuccessfulServiceLog";
+
+    public static final String ID = "id";
+    public static final String SERVICE_ID = "serviceLog";
+    public static final String LAST_ATTEMPT_DATE = "lastAttemptDate";
+    public static final String ALREADY_PASSED_ATTEMPTS_COUNTER = "alreadyPassedAttemptsCounter";
+    public static final String EXCEPTION_NAME = "exceptionName";
+}
--- src/main/java/com/example/todolistapp/service/EmergencyProducerAcknowledgmentListener.java
+++ src/main/java/com/example/todolistapp/service/EmergencyProducerAcknowledgmentListener.java
@@ -2,6 +2,7 @@ package com.example.todolistapp.service;
 
 import com.example.todolistapp.domain.LoggingLevel;
 import com.example.todolistapp.domain.ServiceLog;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -10,15 +11,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class EmergencyProducerAcknowledgmentListener implements ProducerListener<LoggingLevel, ServiceLog> {
+
+    private final ServiceLogManager serviceLogManager;
 
     @Override
     public void onSuccess(ProducerRecord<LoggingLevel, ServiceLog> producerRecord, RecordMetadata recordMetadata) {
-        log.info("Success: ServiceLog {}, Partition : {}, Offset : {}", producerRecord.value(), recordMetadata.partition(), recordMetadata.offset());
+        logSuccess(producerRecord, recordMetadata);
     }
 
     @Override
     public void onError(ProducerRecord<LoggingLevel, ServiceLog> producerRecord, RecordMetadata recordMetadata, Exception exception) {
+        logFailure(producerRecord, exception);
+        serviceLogManager.saveUnsuccessfulServiceLog(producerRecord.value(), exception.getMessage());
+    }
 
+    private void logSuccess(ProducerRecord<LoggingLevel, ServiceLog> producerRecord, RecordMetadata recordMetadata) {
+        log.info("Emergency Log delivery success: ServiceLog {}, Partition : {}, Offset : {}",
+                 producerRecord.value(),
+                 recordMetadata.partition(),
+                 recordMetadata.offset());
+    }
+
+    private void logFailure(ProducerRecord<LoggingLevel, ServiceLog> producerRecord, Exception exception) {
+        log.warn("Emergency Log delivery failure: ServiceLog {}, Partition : {}, Key {}, Timestamp : {}, Exception : {}, Exception message : {}",
+                 producerRecord.value(),
+                 producerRecord.partition(),
+                 producerRecord.key(),
+                 producerRecord.timestamp(),
+                 exception.getCause(),
+                 exception.getMessage()
+        );
     }
 }
